33fcf4bc3b02a9c36ef6a6e2e312655cb995f3eb
Macarena Quiroz Kettle <56402915+Mac-Kett@users.noreply.github.com>
2021-07-13 22:59:43 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app" class="container-fluid mt-3">
+    <div class="jumbotron">
+      <Input />
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Input from "./componentes/Input.vue";
+
+export default {
+  name: "App",
+  components: {
+    Input,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+#app {
+}
+
+h1 {
+  color: orange;
+}
+
+.jumbotron {
+  padding: 32px !important;
+}
+
+hr {
+  color: #222;
+}
+</style>
---
+++ src/componentes/Input.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="src-componentes-entrada">
+    <div class="jumbotron">
+    <h1>Macarena Agustina Quiroz</h1>
+    <h2>Comision: 21D</h2>
+    <h2>Docente: Daniel Sanchez</h2>
+    <br>
+      <h3>Escriba aqui...</h3>
+      <input type="text" v-model="valor" /> <br />
+      <br />
+      {{ valor | codificado }} codificado
+      <br />
+      {{ valor | pasarAmayuscula }} | mayuscula
+      <br />
+      {{ valor | pasarAminuscula }} | minuscula
+      <br />
+      {{ valor | intercalaDesdeMayuscula }} | intercalado iniciado por mayuscula
+      <br />
+      {{ valor | intercalaDesdeMinuscula }} | intercalado iniciado por minuscula
+      <br />
+      {{ cantidadCaracteres }} | cantidad de caracteres ingresados
+    </div>
+   
+
+
+    <p>respuestas: 1:b 2:b 3:c 4:b 5:b</p>
+  </section>
+</template>
+
+<script lang="js">
+  
+  export default  {
+    name: 'src-componentes-entrada',
+    props: [],
+    mounted () {
+
+    },
+    mixins: [],
+    data () {
+      return {
+        valor: '',
+
+      }
+    },
+    methods: {
+       
+    },
+    computed: {
+        cantidadCaracteres(){
+        return this.valor.length
+      }
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+.src-componentes-estructura {
+}
+
+.jumbotron {
+  background-color: white;
+}
+
+hr {
+  background-color: #444;
+}
+</style>
